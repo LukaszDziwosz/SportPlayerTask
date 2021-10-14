afa76aa362980808266a0d74598eec4ad08911cf
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-10-14 21:40:32 +0200

    commit after Xcode crash

--- SportPlayerTests/ListManagerTest.swift
+++ SportPlayerTests/ListManagerTest.swift
@@ -27,10 +27,12 @@ class ListManagerTest: XCTestCase {
             switch result {
             case .success(let events):
                 XCTAssertEqual(events.count, 16)
-                XCTAssertEqual(events[0].id, "1")
+                XCTAssertEqual(events[0].id, "1") 
                 XCTAssertEqual(events[0].title, "Liverpool v Porto")
                 XCTAssertEqual(events[0].subtitle, "UEFA Champions League")
-                
+                XCTAssertEqual(events[0].date, "2021-10-14T01:31:35.989Z")
+                XCTAssertEqual(events[0].imageUrl, "https://firebasestorage.googleapis.com/v0/b/dazn-recruitment/o/310176837169_image-header_pDach_1554579780000.jpeg?alt=media&token=1777d26b-d051-4b5f-87a8-7633d3d6dd20")
+                XCTAssertEqual(events[0].videoUrl, "https://firebasestorage.googleapis.com/v0/b/dazn-recruitment/o/promo.mp4?alt=media")
             case .failure:
                 XCTFail("Expected success")
             }
